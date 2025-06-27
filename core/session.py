527e8d079cfd5b35d513756fd1d6e7aeb9cf3808
Maxmud <maxmudbobomurodov151@gmail.com>
2025-06-27 21:06:42 +0500

    a

--- core/session.py
+++ core/session.py
@@ -4,5 +4,5 @@ from core.config import DB_NAME, DB_HOST, DB_PASS, DB_USER, DB_PORT
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(DATABASE_URL, echo=False)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
